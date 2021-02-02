cdd113d4ad731dd9e10f8060715bbf7e4465ff43
rayhan-abdullah <captainrayhan215@gmail.com>
2021-02-02 02:39:13 -0800

    create ecomarce website

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(function ($) {
+"use strict";
+
+		/* slider-active */
+		
+	$('.slider-active').slick({
+		  infinite: true,
+		  slidesToShow: 1,
+		  arrows: true,
+		  autoplay: false,
+		  slidesToScroll: 1
+	});
+
+
+})(jQuery);
